0f163bc9449355e9411ade6aab7c4e8f57c5a058
Michelle guerrero <mlguerrero@estudiante.uniajc.edu.co>
2026-09-15 09:07:47 -0500

    chore: inicializar proyecto Spring Boot

---
+++ src/main/java/co/edu/uniajc/agrovalle_connect/AgrovalleConnectApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniajc.agrovalle_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrovalleConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrovalleConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uniajc/agrovalle_connect/AgrovalleConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniajc.agrovalle_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrovalleConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
