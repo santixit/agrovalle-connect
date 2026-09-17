68c83dd93bbea1d69257f458da260ba3e87ddc74
santixit <149737045+santixit@users.noreply.github.com>
2026-09-17 00:16:02 -0500

    test(boot): remove duplicate context test
    Removed duplicate Spring Boot test; canonical tests are in co.edu.uniajc.agrovalle.

--- src/test/java/co/edu/uniajc/agrovalle_connect/AgrovalleConnectApplicationTests.java
+++ src/test/java/co/edu/uniajc/agrovalle_connect/AgrovalleConnectApplicationTests.java
@@ -1,13 +1,3 @@
 package co.edu.uniajc.agrovalle_connect;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AgrovalleConnectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+// Duplicate Spring Boot test removed; canonical tests are in co.edu.uniajc.agrovalle.
